c60cb539a1bb64d5c6edc3046406af45cac347f9
Owall <97934878+MayOwall@users.noreply.github.com>
2022-11-22 20:14:37 +0900

    6.3 key

--- 리액트를 다루는 기술_김민준/code_practice/src/IterationSample.js
+++ 리액트를 다루는 기술_김민준/code_practice/src/IterationSample.js
@@ -1,6 +1,8 @@
 const IterationSample = () => {
     const members = ["짱구", "철수", "맹구", "유리", "훈이"];
-    const memberList = members.map((member) => <li>{member}</li>);
+    const memberList = members.map((member, idx) => (
+        <li key={idx}>{member}</li>
+    ));
     return <ul>{memberList}</ul>;
 };
 
