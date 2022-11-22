e62a3f26c1d39ad92b2d001d7b1fef0f75a79cf3
Owall <97934878+MayOwall@users.noreply.github.com>
2022-11-22 20:06:21 +0900

    5.3 컴포넌트에 ref 달기

--- 리액트를 다루는 기술_김민준/code_practice/src/App.js
+++ 리액트를 다루는 기술_김민준/code_practice/src/App.js
@@ -1,9 +1,26 @@
 import { Component } from "react";
+import ScrollBox from "./ScrollBox";
 import ValidationSample from "./ValidationSample";
 
 class App extends Component {
     render() {
-        return <ValidationSample />;
+        return (
+            <>
+                <ScrollBox
+                    ref={(ref) => {
+                        this.scrollBox = ref;
+                    }}
+                />
+                <button
+                    onClick={() => {
+                        this.scrollBox.handleScroll();
+                    }}
+                >
+                    스크롤을 내리는 버튼
+                </button>
+                <ValidationSample />;
+            </>
+        );
     }
 }
 export default App;
