cf7754f97953020577756f61ba6b48ed50571c55
Owall <97934878+MayOwall@users.noreply.github.com>
2022-11-22 20:27:52 +0900

    6.4 응용

--- 리액트를 다루는 기술_김민준/code_practice/src/IterationSample.js
+++ 리액트를 다루는 기술_김민준/code_practice/src/IterationSample.js
@@ -1,9 +1,46 @@
+import { useState } from "react";
+
 const IterationSample = () => {
-    const members = ["짱구", "철수", "맹구", "유리", "훈이"];
-    const memberList = members.map((member, idx) => (
-        <li key={idx}>{member}</li>
+    const [names, setNames] = useState([
+        { id: 1, content: "짱구" },
+        { id: 2, content: "철수" },
+        { id: 3, content: "맹구" },
+        { id: 4, content: "유리" },
+        { id: 5, content: "훈이" },
+    ]);
+
+    const [inputText, setInputText] = useState("");
+
+    const [nextId, setNextId] = useState(6);
+
+    const onChange = (e) => setInputText(e.target.value);
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId,
+            content: inputText,
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText("");
+    };
+    const onRemove = (id) => {
+        const nextNames = names.filter((name) => name.id !== id);
+        setNames(nextNames);
+    };
+
+    const nameList = names.map((name) => (
+        <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+            {name.content}
+        </li>
     ));
-    return <ul>{memberList}</ul>;
+
+    return (
+        <>
+            <input value={inputText} onChange={onChange} />
+            <button onClick={onClick}>이름 추가</button>
+            <ul>{nameList}</ul>
+        </>
+    );
 };
 
 export default IterationSample;
