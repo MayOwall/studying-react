19e60dd3a0d5aadd44d8cf984856c7e0222ed6f1
pleand <pleand@pleandui-MacBookPro.local>
2022-11-23 19:22:34 +0900

    8.1 useState

---
+++ 리액트를 다루는 기술_김민준/code_practice2/src/Info.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const Info = () => {
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div> 이름 : {name}</div>
+      <div> 이름 : {nickname}</div>
+    </div>
+  );
+};
+
+export default Info;
