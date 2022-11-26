aef97c6d91c36fb0620702899caf7250244713d1
MayOwall <pixartive@gmail.com>
2022-11-26 11:31:10 +0900

    11. 최적화

--- 리액트를 다루는 기술_김민준/code_practice_todoapp/src/App.js
+++ 리액트를 다루는 기술_김민준/code_practice_todoapp/src/App.js
@@ -4,54 +4,44 @@ import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import './App.css';
 
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '드뷔시 알아보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '에리히 프롬 알아보기',
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
       checked: false,
-    },
-  ]);
-  const nextId = useRef(4);
+    });
+  }
+  return array;
+}
 
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos],
-  );
+function App() {
+  const [todos, setTodos] = useState(createBulkTodos);
+  const nextId = useRef(2501);
+
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos((todos) => todos.concat(todo));
+    nextId.current += 1;
+  }, []);
 
   const onRemove = useCallback(
-    (id) => setTodos(todos.filter((todo) => todo.id !== id)),
-    [todos],
+    (id) => setTodos((todos) => todos.filter((todo) => todo.id !== id)),
+    [],
   );
 
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
+  const onToggle = useCallback((id) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
+  }, []);
 
   return (
     <TodoTemplate>
